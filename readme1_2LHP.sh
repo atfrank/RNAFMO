4198f7af10791db048a99acb2a76639a76c69b5c
Hazel Wong <hazwong@umich.edu>
2019-11-16 17:28:24 -0500

    reorganizing

--- readme1_2LHP.sh
+++
@@ -1,6 +0,0 @@
-COORS=/home/hazwong/test/git_repository/RNAFMO/2LHP_0/
-frames=`seq 1 1 6`
-for frame in $frames
-do
-    utility/./prepare_database.sh ${COORS}/2LHP_${frame}.pdb 2LHP_${frame} 100 charmm 1  tmp/    
-done
