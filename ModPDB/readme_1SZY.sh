1b63133cd724b3852e9325d184f65612b8273f0b
Hazel Wong <hazwong@umich.edu>
2019-11-17 21:16:35 -0500

    added TER and END to all NMR molecules

--- ModPDB/readme_1SZY.sh
+++
@@ -1,6 +0,0 @@
-COORS= /home/colltac/RNAFMO/1SZY_0
-frames=`seq 1 1 6`
-for frame in $frames
-do
-    utility/./prepare_database.sh ${COORS}/1SZY_${frame}.pdb 1SZY_${frame} 100 charmm 1  tmp/    
-done
